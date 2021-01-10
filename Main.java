0562554e1d1a3a0b698a6d1dc32079416b4a3242
Evan D <Evan_delasota@yahoo.com>
2021-01-10 06:22:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+class Main {
+  public static void main(String[] args) {
+    int[] arr = {1, 5, 1, 10, 13, 9, 40, 33, 28, 15};
+    
+    System.out.print("Unsorted: ");
+    for (int n : arr) {
+      System.out.print(n + " ");
+    }
+    System.out.println();
+
+    
+    System.out.print("Sorted:   ");
+    for (int n : arr) {
+      System.out.print(n + " ");
+    }
+    System.out.println();
+
+  }
+}
